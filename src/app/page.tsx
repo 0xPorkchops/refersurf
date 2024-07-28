c6542f506a9f949f54b73e30056be94b3dc09b0d
0xPorkchops <proulxcameron@icloud.com>
2024-07-28 01:06:48 -0400

    Updated schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,13 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/ef8a8d21-2d4e-4090-b149-95f4a650536a-wypifl.jpg",
-  "https://utfs.io/f/a3cb0d08-4f7e-4e45-985a-30932318ce23-ofa50y.jpg",
-  "https://utfs.io/f/07f9c2e0-e11d-4272-9dda-b1d449e3f4a9-e9pxax.jpg"
-]
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url
-}));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-
-  console.log(posts);
+  const images = await db.query.images.findMany();
   
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+        {[...images, ...images, ...images].map((image, index) => (
           <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} />
           </div>
