4398beaf69a821607047f10c166ff3ea094ba235
0xPorkchops <proulxcameron@icloud.com>
2024-07-30 13:10:02 -0400

    Added Next image optimization component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
+import Image from "next/image"
 import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
@@ -8,10 +9,10 @@ async function Images() {
   const images = await getMyImages();
 
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap justify-center gap-4">
     {images.map((image) => (
-      <div key={image.id} className="w-48 flex flex-col">
-        <img src={image.url} />
+      <div key={image.id} className="flex h-48 w-48 flex-col">
+        <Image src={image.url} style={{ objectFit: "contain" }} width ={192} height={192} alt={image.name} />
         <div>{image.name}</div>
       </div>
     ))}
