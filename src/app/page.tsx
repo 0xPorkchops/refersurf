6ad8b4e5d8a69d401afe4524874b124f2159c6a0
0xPorkchops <proulxcameron@icloud.com>
2024-07-31 23:08:47 -0400

    Started implementing modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 import Image from "next/image"
 import { getMyImages } from "~/server/queries";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
@@ -12,7 +13,9 @@ async function Images() {
     <div className="flex flex-wrap justify-center gap-4">
     {images.map((image) => (
       <div key={image.id} className="flex h-48 w-48 flex-col">
-        <Image src={image.url} style={{ objectFit: "contain" }} width ={192} height={192} alt={image.name} />
+        <Link href={`/img/${image.id}`}>
+          <Image src={image.url} style={{ objectFit: "contain" }} width ={192} height={192} alt={image.name} />
+        </Link>
         <div>{image.name}</div>
       </div>
     ))}
